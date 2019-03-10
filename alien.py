9158c3367cdbda9461aaa6a2fb8aeb3761dcc347
vanja-abakumov <vanja.abakumov@gmail.com>
2019-03-10 13:26:35 +0300

    Мигающий инопланетянин

--- alien.py
+++ alien.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 
 
 # Рисует координатные оси
@@ -11,6 +12,16 @@ def draw_axis(canvas):
         canvas.create_text(i, 30, text=str(i), fill="purple", font=("Helvectica", "10"))
         canvas.create_text(30, i, text=str(i), fill="purple", font=("Helvectica", "10"))
         canvas.create_line(10, 10, 10, 500, width=3, arrow=LAST)
+
+
+def blink(canvas, fig1, fig2):
+    canvas.itemconfig(fig1, fill='green')
+    canvas.itemconfig(fig2, state=HIDDEN)
+
+
+def unblink(canvas, fig1, fig2):
+    canvas.itemconfig(fig1, fill='white')
+    canvas.itemconfig(fig2, state=NORMAL)
 
 
 size = 500
@@ -27,5 +38,13 @@ eyeball = draw.create_oval(190, 90, 210, 110, fill="black")
 mouth = draw.create_oval(150, 220, 250, 240, fill="red")
 neck = draw.create_line(200, 150, 200, 130, width=10)
 hat = draw.create_polygon(180, 75, 220, 75, 200, 20, fill='blue')
+words = draw.create_text(200, 280, text='я  идиот')
 
-win.mainloop()
+while True:
+    blink(draw, eye, eyeball)
+    win.update()
+    time.sleep(0.5)
+
+    unblink(draw, eye, eyeball)
+    win.update()
+    time.sleep(0.5)
